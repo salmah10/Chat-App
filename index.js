cf683447ac31672c63b938e703d3c8b1721611c0
Vivian <vivnguyen1205@gmail.com>
2024-10-25 16:03:29 -0400

    adding firebase

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as functions from "firebase-functions";
+const functions = require("firebase-functions");
+exports.createChatEngineUser = functions.auth.user().onCreate((user) => {
+  console.log("create", user);
+});
+
+exports.deleteChatEngineUser = functions.auth.user().onDelete((user) => {
+  console.log("delete", user);
+});
